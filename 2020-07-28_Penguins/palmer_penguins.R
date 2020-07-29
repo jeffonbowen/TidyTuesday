2c95a584d911a374937aa31f7e24c5b815b2ef89
Jeff Matheson <jeff@capstoneenvironmental.ca>
2020-07-29 14:56:54 -0700

    Updates

--- 2020-07-28_Penguins/palmer_penguins.R
+++ 2020-07-28_Penguins/palmer_penguins.R
@@ -2,7 +2,10 @@
 
 library(tidyverse)
 library(tidytuesdayR)
-library(ggpairs)
+library(GGally)
+library(vegan)
+library(rpart)
+library(rpart.plot)
 
 
 tuesdata <- tidytuesdayR::tt_load('2020-07-28')
@@ -34,12 +37,72 @@ penguins %>%
   facet_grid(species ~ .)
 
 
-
+ggpairs(data = penguins, columns = 3:6, ggplot2::aes(colour = species))
 
 
 # Scatter
 penguins %>% 
   ggplot(aes(bill_length_mm, bill_depth_mm, colour = sex)) +
   geom_point() +
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm") +
   facet_grid(species ~ .)
+
+penguins %>% 
+  ggplot(aes(bill_length_mm, bill_depth_mm, colour = sex, size = body_mass_g)) +
+  geom_point() +
+  facet_grid(species ~ .)
+
+
+# Questions
+  # Can you predict the species based on the morphometric data?
+
+# Try NMDS
+pengv <- penguins %>% 
+  select(3:6)
+
+nmds <- metaMDS(pengv, distance = "bray")
+
+# extract NMDS scores (x and y coordinates)
+data.scores = as.data.frame(scores(nmds))
+
+# add columns to data frame 
+data.scores$species <- penguins$species
+data.scores$sex <- penguins$sex
+
+n_plot <- ggplot(data.scores, aes(x = NMDS1, y = NMDS2)) + 
+  geom_point(size = 2, aes(shape = sex, colour = species))+ 
+  theme(axis.text.y = element_text(colour = "black", size = 10, face = "bold"), 
+        axis.text.x = element_text(colour = "black", face = "bold", size = 10), 
+        legend.text = element_text(size = 10, face ="bold", colour ="black"), 
+        legend.position = "right", axis.title.y = element_text(face = "bold", size = 11), 
+        axis.title.x = element_text(face = "bold", size = 10, colour = "black"), 
+        legend.title = element_text(size = 11, colour = "black", face = "bold"), 
+        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
+        legend.key=element_blank()) + 
+  labs(x = "NMDS1", colour = "Species", y = "NMDS2", shape = "Sex") 
+n_plot
+
+ggsave(plot = n_plot, "nmds_plot.png")
+
+# How about a predictor tool?
+
+# Approach from https://github.com/jack-davison/TidyTuesday/blob/master/R/2020_07_28_Penguins.R
+
+fitr <- rpart(data = penguins, formula = species ~ bill_length_mm + bill_depth_mm, 
+              flipper_length_mm + body_mass_g)
+fitr
+
+summary(fitr)
+
+# Print table of optimal prunings
+printcp(fitr)
+
+plot(fitr)
+text(fitr)
+
+tree <- rpart.plot(fitr, 
+                   type = 5, 
+                   under = TRUE, 
+                   fallen.leaves = TRUE)
+
+ggsave(data = tree, file = "penguin_class_tree.png")
--- README.md
+++ README.md
@@ -6,5 +6,8 @@ This is where I keep my TidyTuesday practice.
 
 # 2020-07-28 Palmer Penguins
 
+## Visualizing morphometric differences among three penguin species using Non-metric Multidimensional Scaling 
 ![./2020-07-27_penguins/nmds_plot.png](https://github.com/jeffonbowen/TidyTuesday/blob/master/nmds_plot.png)
 
+## Classification of three penguin species using regression trees
+![./2020-07-27_penguins/palmer_penguins_tree.png](https://github.com/jeffonbowen/TidyTuesday/blob/master/palmer_penguins_tree.png)
